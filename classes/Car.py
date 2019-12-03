f4919a96891ce71ddb5255ad258a1a2e2bbedf12
Adam Pócs <adam.pocs@edreamsodigeo.com>
2019-12-03 21:16:39 +0100

    Continue working with classes

--- classes/Car.py
+++ classes/Car.py
@@ -3,19 +3,21 @@
 import json
 
 class Car:
-	def __init__(self, car_name, car_color, car_type):
+	def __init__(self, car_name, car_color):
 		self.car_name = car_name
-		self.car_color = car_color
-		self.car_type = car_type
 		with open("settings.json", "r") as read_file: self.CARS = json.load(read_file)
 		read_file.close()
+		if (car_color in self.CARS[car_name]["color"]):
+			self.car_color = car_color
+		else:
+			self.car_color = "default"
+	
 
 	def go(self, distance):
 		return(self.car_name + '.'*distance)
 
 	def get_number_of_doors(self):
 		car_name = self.car_name
-		# in_cars = any(x["name"] == car_type for x in self.CARS["items"])
 		if (car_name in self.CARS):
 			number_of_doors = self.CARS[car_name]["doors"]
 			return number_of_doors
--- classes/__init__.py
+++ classes/__init__.py
@@ -3,8 +3,9 @@
 from car import Car
 
 def main():
-	my_first_car = Car('ymdbdfb', 'red', 'bmw')
+	my_first_car = Car('audi', 'jsdhjdfhg')
 	print(my_first_car.get_number_of_doors())
+	print(my_first_car.car_color)
 
 if __name__ == '__main__':
 	main()
