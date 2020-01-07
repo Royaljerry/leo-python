1b27417d9efa788302dbde66d3b32c1efac29f01
Adam Pócs <adam.pocs@edreamsodigeo.com>
2020-01-07 16:54:55 +0100

    Import data json

--- monstars/__init__.py
+++ monstars/__init__.py
@@ -1,3 +1,12 @@
 #!/Users/adam.pocs/.pyenv/shims/python
 
-print('monsters')
+import json
+
+data = ''
+
+if __name__ == '__main__':
+	print('monsters running')
+	with open("data/data.json", "r") as read_file:
+		data = json.load(read_file)
+		read_file.close()
+	print(data)
