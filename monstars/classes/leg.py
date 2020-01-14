a5c00bacd3a85426ce2c8bf75e969fa25d6cea02
Horváth Leó <horvath.leo2001@gmail.com>
2020-01-14 21:26:15 +0100

    create data representation

--- monstars/classes/leg.py
+++ monstars/classes/leg.py
@@ -1,5 +1,14 @@
 import random
+
+data_representation = {
+	'side':['0011'] ,
+	'front':['0110']
+}
 class Leg:
 	def __init__(self):
 		self.direction = random.randint(0, 2)
+		if self.direction == 2 or self.direction == 0:
+			self.direction = data_representation['front']
+		else: 
+			self.direction = data_representation['side']
 		print('leg created with direction ', self.direction)
