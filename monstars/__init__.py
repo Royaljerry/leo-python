cb0aa496186bc25d4f3543580890ec274c0d4d9c
Horváth Leó <horvath.leo2001@gmail.com>
2020-01-28 21:33:42 +0100

    reverse leg

--- monstars/__init__.py
+++ monstars/__init__.py
@@ -17,6 +17,6 @@ def main():
 	create_monsters(data['dimensions'][0], data['dimensions'][1])
 
 if __name__ == '__main__':
-	str = "Horváth"
-	print(str[::-1])
+	# str = "Horváth"
+	# print(str[::-1])
 	main()
--- monstars/classes/leg.py
+++ monstars/classes/leg.py
@@ -7,17 +7,18 @@ class Leg:
 		}
 
 		self.direction = random.randint(0, 2)
-		if self.direction == 2 or self.direction == 0:
-			self.direction = self.data_representation['side']
-			print('normal: ' + str(self.direction))
-			print('reversed: ' + str(self.reverseString(self.direction)))
-		else: 
+		if self.direction == 0:
+			self.direction = 'normal: ' + str(self.data_representation['side'])
+		elif self.direction == 2:
+			self.direction = 'reversed: ' + str(self.reverseString(self.data_representation['side']))
+		elif self.direction == 1:
 			self.direction = self.data_representation['front']
+
 		print('leg created with direction ', self.direction)
-	#print(calculate_direction(direction, data_representation))
+		print('')
 	
-	def calculate_direction(self, direction, representation):
-		print(direction)
+	# def calculate_direction(self, direction, representation):
+	# 	print(direction)
 
 	def reverseString(self, string):
 		return string[::-1]
