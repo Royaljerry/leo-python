c7afc4cb64d3b2a6600b141949dff30b1b8a4167
Adam Pócs <adam.pocs@edreamsodigeo.com>
2020-01-28 20:14:54 +0100

    Fix leg data_representation reference

--- monstars/classes/leg.py
+++ monstars/classes/leg.py
@@ -1,14 +1,18 @@
 import random
-
-data_representation = {
-	'side':['0011'] ,
-	'front':['0110']
-}
 class Leg:
 	def __init__(self):
+		self.data_representation = {
+			'side': ['0011'] ,
+			'front': ['0110']
+		}
+
 		self.direction = random.randint(0, 2)
 		if self.direction == 2 or self.direction == 0:
-			self.direction = data_representation['front']
+			self.direction = self.data_representation['front']
 		else: 
-			self.direction = data_representation['side']
+			self.direction = self.data_representation['side']
 		print('leg created with direction ', self.direction)
+	#print(calculate_direction(direction, data_representation))
+	
+	def calculate_direction(self, direction, representation):
+		print("sel.direction: ", direction)
