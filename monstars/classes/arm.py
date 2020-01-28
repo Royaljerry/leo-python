ebd5352752690392fa6f34aeccb0e967cf8233e2
Horváth Leó <horvath.leo2001@gmail.com>
2020-01-28 20:35:23 +0100

    finish random generations

--- monstars/classes/arm.py
+++ monstars/classes/arm.py
@@ -2,5 +2,15 @@ import random
 
 class Arm:
 	def __init__(self):
+		
+		self.data_representation = {
+			'curved': ['0010'] ,
+			'straight': ['0011']
+		}
+
 		self.direction = random.randint(0, 2)
+		if self.direction == 2 or self.direction == 0:
+			self.direction = self.data_representation['curved']
+		else: 
+			self.direction = self.data_representation['straight']
 		print('leg created with direction ', self.direction)
--- monstars/classes/head.py
+++ monstars/classes/head.py
@@ -2,5 +2,15 @@ import random
 
 class Head:
 	def __init__(self):
+
+		self.data_representation = {
+			'side': ['0011'] ,
+			'front': ['0110']
+		}
+
 		self.direction = random.randint(0, 2)
+		if self.direction == 2 or self.direction == 0:
+			self.direction = self.data_representation['front']
+		else: 
+			self.direction = self.data_representation['side']
 		print('head created with direction ', self.direction)
