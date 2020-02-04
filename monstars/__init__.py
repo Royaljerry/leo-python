a48c147931dcb28170ff4a75ad6f67a1dd6ff12b
Horváth Leó <horvath.leo2001@gmail.com>
2020-02-04 18:54:37 +0100

    store monsters

--- monstars/__init__.py
+++ monstars/__init__.py
@@ -11,17 +11,20 @@ monsterDraw = ImageDraw.Draw(monsterImage)
 monsterImage.show()
 
 data = ''
+monsters = []
 
 def create_monsters(monsters_hor, monsters_ver):
 	for row in range(monsters_ver):
 		for column in range(monsters_hor):
 			monster = Monster()
+			monsters.append(monster)
 
 def main():
 	with open("data/data.json", "r") as read_file:
 		data = json.load(read_file)
 		read_file.close()
 	create_monsters(data['dimensions'][0], data['dimensions'][1])
+	print (monsters)
 
 if __name__ == '__main__':
 	main()
